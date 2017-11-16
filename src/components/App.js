987cb3855653c78b910656ebdcfe489b12bdc338
cerico <garethrobertlee@gmail.com>
2017-11-16 11:29:59 +0000

    show base currency in view

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { getRates } from '../actions/currency';
 
 class App extends React.Component {
+
+  componentWillMount(){
+    this.props.getRates()
+  }
 
   render () {
     return (
       <div>
-          hello 
+          BASE: {this.props.base}
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return ({
+      rates: state.rates.rates,
+      result: state.rates.result,
+      base: state.rates.base,
+      target: state.rates.target,
+      amount: state.rates.amount
+    })
+}
+
+export default connect(mapStateToProps, {getRates})(App);
