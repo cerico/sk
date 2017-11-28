56d0fb03fb687ac856706381b9c175c361af4b13
cerico <garethrobertlee@gmail.com>
2017-11-28 14:51:26 +0000

    refactor base webpack

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -2,42 +2,23 @@ module.exports = options => {
     return {
       entry: './src/index.js',
       module: {
-        rules: [
-          {
+        rules: [{
               test: /\.js$/,
               loader: 'babel-loader',
               exclude: /node_modules/,
           }, {
-              test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-              loader: 'file-loader'
-          }, {
-              test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+              test: /\.(png|jpe?g|gif|svg|eot|svg|ttf|woff|woff2)(\?\S*)?$/,
               loader: 'file-loader',
-              query: {
-                  name: '[name].[ext]?[hash]'
-              }
+              query: {name: '[name].[ext]?[hash]'}
           }, {
-              test: /\.css$/,
+              test: /\.css|scss$/,
               use: [
-                'style-loader',
-                'css-loader'
+                'style-loader','css-loader','sass-loader'
               ]
-            },
-            {
-              test: /\.scss$/,
-              use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-              ]
-            },   {
+            },  {
               test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
               loader: 'url-loader?limit=1000000&mimetype=application/font-woff'
-          }, {
-              test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-              loader: 'file-loader'
-          },
-      ],
+          }],
       },
     }
   }
