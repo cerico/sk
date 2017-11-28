801e45885ca4f8ca2997cd7f20c5bf3145f9613f
garethrobertlee <garethrobertlee@garethrobertlees-MacBook-Air.local>
2017-11-28 09:56:07 +0000

    prod version

---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+module.exports = options => {
+    return {
+      entry: './src/index.js',
+      output: {
+        path: path.join(__dirname, '..', 'dist'),
+        filename: 'bundle.js',
+      },
+      module: {
+        rules: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+          }, {
+              test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+              loader: 'file-loader'
+          }, {
+              test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+              loader: 'file-loader',
+              query: {
+                  name: '[name].[ext]?[hash]'
+              }
+          }, {
+              test: /\.css$/,
+              use: [
+                'style-loader',
+                'css-loader'
+              ]
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+              ]
+            },   {
+              test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+              loader: 'url-loader?limit=1000000&mimetype=application/font-woff'
+          }, {
+              test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+              loader: 'file-loader'
+          },
+      ],
+      },
+    }
+  }
+  
